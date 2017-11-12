fa192d0322d66c3f36d0462bc4fa8bd310c2ea5e
phyfrancis <neptunefrancis@gmail.com>
2017-11-11 19:48:04 -0800

    added files

---
+++ analysis/group2_question1/query.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+### Make sure you already run 'inject_mysql.sh' beforehand,
+### that script populates the db table.
+
+origin=$1
+
+mysql -uroot -proot << EOF
+USE capstone;
+select * from departure_delay_by_origin_by_airline
+where origin = "$origin"
+order by delay
+limit 10
+EOF
---
+++ analysis/group2_question2/inject_mysql.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+### pre-step: download the data from aws s3 by running
+### $ aws configure [one time setup for credential]
+### $ aws s3 sync s3://daiqian.capstone/group2_question2
+
+table_name=departure_delay_by_origin_by_dest
+data_src=$(pwd)/data
+
+mysql -uroot -proot << EOF
+CREATE DATABASE IF NOT EXISTS capstone;
+USE capstone;
+DROP TABLE IF EXISTS $table_name;
+CREATE TABLE $table_name (
+  origin CHAR(20),
+  dest CHAR(20),
+  delay DECIMAL(10,6)
+);
+EOF
+
+for f in $(ls $data_src) ; do
+  cat $data_src/$f | awk 'BEGIN {FPAT="([^ \t]+)|(\"[^\"]+\")"} {print $1,$2,$3}' > tmp
+
+mysql -uroot -proot << EOF
+USE capstone;
+LOAD DATA LOCAL INFILE "$(pwd)/tmp"
+INTO TABLE $table_name
+COLUMNS TERMINATED BY ' '
+OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\n';
+EOF
+
+done
+
+rm -f tmp
