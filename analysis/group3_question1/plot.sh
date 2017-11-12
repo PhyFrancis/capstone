fb82cf3ff18eccb689faa4f9cb089f85918d364f
phyfrancis <neptunefrancis@gmail.com>
2017-11-11 20:17:01 -0800

    nit

--- analysis/group3_question1/plot.sh
+++ analysis/group3_question1/plot.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
-aws s3 sync s3://daiqian.capstone/group1_question1 .
-filename="part-r-00000"
-n=$(wc -l < $filename)
+aws s3 sync s3://daiqian.capstone/group1_question1 data
+filepath="data/part-r-00000"
+n=$(wc -l < $filepath)
 echo $n
 
 gnuplot --persist << EOF
   set logscale xy
   set xrange [1:1e8]
   set yrange [0.01:1]
-  plot "$filename" using 1:(column(0)/$n)
+  plot "$filepath" using 1:(column(0)/$n)
 EOF
