0dc38934e1c08e325ab466e33454714a6fff7a89
phyfrancis <neptunefrancis@gmail.com>
2017-11-14 21:51:01 -0800

    example is more realistic

--- analysis/group3_question2/cql.sh
+++ analysis/group3_question2/cql.sh
@@ -1,8 +1,14 @@
 #! /bin/bash
 
 ## TODO in MapReduce code:
-## 1. use '|' as delimiter
+## 1. use '|' as delimiter, and please remove any spaces around '|'.
 ## 2. get rid of quote in A-B-C key field.
+
+## Caveat: (The following are true unless allow filtering, which sucks)
+## 1. Cassandra only allows query on primary key
+## 2. When using compound primary key, the column can only be restricted if
+##    preceeding column is restricted.
+## 3. range query is not supported.
 
 password=iYDdgdNX0Vnu
 
@@ -14,11 +20,17 @@ USE capstone;
 
 DROP TABLE IF EXISTS connecting_flights;
 CREATE TABLE connecting_flights (
-  id text PRIMARY KEY,
-  delay double
+  date timestamp,
+  airport_a text,
+  airport_b text,
+  airport_c text,
+  delay double,
+  first_leg text,
+  second_leg text,
+  PRIMARY KEY ((date), airport_a, airport_b, airport_c)
 );
 
 COPY connecting_flights FROM 'data.txt' with DELIMITER = '|';
-select * from connecting_flights;
+SELECT * from connecting_flights WHERE date = '2008-08-01' AND airport_a = 'ORD';
 
 EOF
