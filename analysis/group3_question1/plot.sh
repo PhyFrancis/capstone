99defe9f1d064706f3425ec00bd33896b59e87e2
phyfrancis <neptunefrancis@gmail.com>
2018-01-14 15:17:31 -0800

    nit

--- analysis/group3_question1/plot.sh
+++ analysis/group3_question1/plot.sh
@@ -3,11 +3,12 @@
 aws s3 sync s3://daiqian.capstone/group1_question1 data
 filepath="data/part-r-00000"
 n=$(wc -l < $filepath)
-echo $n
 
 gnuplot --persist << EOF
   set logscale xy
   set xrange [1:1e8]
   set yrange [0.01:1]
-  plot "$filepath" using 1:(column(0)/$n)
+  set xlabel 'Airport Usage Count'
+  set ylabel 'CDF'
+  plot "$filepath" using 1:(column(0)/$n) notitle
 EOF
