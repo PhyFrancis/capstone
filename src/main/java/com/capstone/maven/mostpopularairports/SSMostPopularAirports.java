48d3f09158a7b24cdec8e898fde2052e7b1f5145
phyfrancis <neptunefrancis@gmail.com>
2018-01-14 17:27:34 -0800

    spark streaming wip

---
+++ src/main/java/com/capstone/maven/mostpopularairports/SSMostPopularAirports.java
@@ -0,0 +1,38 @@
+package com.capstone.maven.mostpopularairports;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.*;
+import org.apache.spark.api.java.function.*;
+
+import scala.Tuple2;
+
+public class SSMostPopularAirports {
+
+	public static void main(String[] args) {
+		JavaSparkContext sc = new JavaSparkContext(new SparkConf().setAppName("Flight Count"));
+
+		JavaRDD<String> tokenized = sc.textFile(args[0]).flatMap(new FlatMapFunction<String, String>() {
+			public Iterator<String> call(String s) {
+				return (Iterator<String>) Arrays.asList(s.split(" "));
+			}
+		});
+		
+		// count the occurrence of each word
+	    JavaPairRDD<String, Integer> counts = tokenized.mapToPair(
+	      new PairFunction<String, String, Integer>() {
+	        public Tuple2<String, Integer> call(String s) {
+	          return new Tuple2<String, Integer>(s, 1);
+	        }
+	      }
+	    ).reduceByKey(
+	      new Function2<Integer, Integer, Integer>() {
+	        public Integer call(Integer i1, Integer i2) {
+	          return i1 + i2;
+	        }
+	      }
+	    );
+	}
+}
