432717860c67ae1ef7eec5132b4d1b1a453a1b1a
Daiqian Zhang <neptunefrancis@gmail.com>
2018-02-04 01:59:24 +0000

    implemented data cleaner

--- task2_spark_streaming/src/main/scala/DataCleaner.scala
+++ task2_spark_streaming/src/main/scala/DataCleaner.scala
@@ -1,9 +1,55 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 
+// ========= Raw data schema ==========
+// Delimiter is ":::"
+//0   StructField("YEAR", NumericType),
+//1   StructField("MONTH", NumericType),
+//2   StructField("DAY_OF_MONTH", NumericType),
+//3   StructField("DAY_OF_WEEK", NumericType),
+//4   StructField("FLIGHT_DATE", DateType),
+//5   StructField("UNIQUE_CARRIER", StringType),
+//6   StructField("AIRLINE_ID", NumericType),
+//7   StructField("CARRIER", StringType),
+//8   StructField("FLIGHT_NUM", StringType),
+//9   StructField("ORIGIN", StringType),
+//10  StructField("DEST", StringType),
+//11  StructField("CRS_DEP_TIME", StringType),
+//12  StructField("DEP_DELAY", NumericType),
+//13  StructField("CRS_ARR_TIME", StringType),
+//14  StructField("ARR_DELAY", NumericType),
+//15  StructField("CANCELLED", NumericType),
+//16  StructField("DIVERTED", NumericType),
+//17  StructField("FLIGHTS", NumericType)
+
+// ========== CLeaned data schema ==========
+// TODO fill in here
+// Delimiter is ":::"
+//    StructField("FLIGHT_DATE", DateType),
+//    StructField("UNIQUE_CARRIER", StringType),
+//    StructField("FLIGHT_NUM", StringType),
+//    StructField("ORIGIN", StringType),
+//    StructField("DEST", StringType),
+//    StructField("CRS_DEP_TIME", StringType),
+//    StructField("DEP_DELAY", NumericType),
+//    StructField("CRS_ARR_TIME", StringType),
+//    StructField("ARR_DELAY", NumericType),
+//    StructField("CANCELLED", NumericType),
+//    StructField("DIVERTED", NumericType),
+
 object DataCleaner {
   def main(args: Array[String])  {
+    val inputPath = args(0)
+    val outputPath = args(1)
+
     val conf = new SparkConf().setAppName("data cleaner")
     val sc = new SparkContext(conf)
+
+    val selectIndex = Array(4,5,8,9,10,11,12,13,14,15,16)
+    sc.textFile(inputPath)
+      .map{line =>
+        val fields = line.split(":::")
+        selectIndex.map(i => fields(i)).mkString("|").replace("\"","")
+      }.saveAsTextFile(outputPath)
   }
 }
--- task2_spark_streaming/submit.sh
+++ task2_spark_streaming/submit.sh
@@ -1,6 +1,17 @@
 #! /bin/bash
 
 spark_home=/home/ubuntu/spark-2.2.1-bin-hadoop2.7
+jar_local=/home/ubuntu/spark-2.2.1-bin-hadoop2.7/work/capstone/task2_spark_streaming/target/scala-2.11/capstonetask2_2.11-1.0.jar
+jar_hdfs=hdfs://ip-172-31-5-186:9000/jars/capstonetask2_2.11-1.0.jar
+
+# Data cleaning job
+${spark_home}/bin/spark-submit \
+  --class DataCleaner \
+  --master spark://ip-172-31-5-186.ec2.internal:6066 \
+  --deploy-mode cluster \
+  ${jar_hdfs} \
+  hdfs://ip-172-31-5-186:9000/raw_data/* \
+  hdfs://ip-172-31-5-186:9000/cleaned_data
 
 # hdfs dfs -rm -r /result/g1q1
 # ${spark_home}/bin/spark-submit \
@@ -10,9 +21,3 @@ spark_home=/home/ubuntu/spark-2.2.1-bin-hadoop2.7
 #   hdfs://ip-172-31-5-186:9000/jars/g1q1_2.11-1.0.jar \
 #   hdfs://ip-172-31-5-186:9000/cleaned_data/* \
 #   hdfs://ip-172-31-5-186:9000/result/g1q1
-
-${spark_home}/bin/spark-submit \
-  --class G1Q1 \
-  --master local[4] \
-  ~/spark-2.2.1-bin-hadoop2.7/work/g1q1/target/scala-2.11/g1q1_2.11-1.0.jar \
-  /home/ubuntu/spark-2.2.1-bin-hadoop2.7/work aaa
