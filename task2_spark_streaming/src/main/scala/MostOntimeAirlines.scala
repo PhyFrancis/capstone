830241e43c9e5084621ce0e0e3842255157f6972
Daiqian Zhang <neptunefrancis@gmail.com>
2018-02-17 19:34:52 +0000

    stateful streaming works

--- task2_spark_streaming/src/main/scala/MostOntimeAirlines.scala
+++ task2_spark_streaming/src/main/scala/MostOntimeAirlines.scala
@@ -3,17 +3,28 @@ package capstone
 import com.datastax.spark.connector.cql.CassandraConnector
 import com.datastax.spark.connector.streaming._
 import kafka.serializer.StringDecoder
+import org.apache.hadoop.fs.FileSystem
+import org.apache.hadoop.fs.Path
 import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
 import org.apache.spark.streaming.{StreamingContext,Seconds,StateSpec,State}
 import org.apache.spark.streaming.kafka._
+
+import java.net.URI
 
 case class OntimeSummary(count: Int, total_delay: Double)
 
 object G1Q2 {
   def main(args: Array[String]) {
     val sparkConf = new SparkConf().setAppName("most ontime airlines")
-    val ssc = new StreamingContext(sparkConf, Seconds(10))
+    val sc = new SparkContext(sparkConf)
+    val ssc = new StreamingContext(sc, Seconds(10))
     ssc.checkpoint("hdfs://ip-172-31-5-186:9000/G1Q2_tmp")
+    
+    val fs = FileSystem.get(new URI("hdfs://ip-172-31-5-186:9000"), sc.hadoopConfiguration);
+    if(fs.exists(new Path("/G1Q2_tmp"))) {
+      fs.delete(new Path("/G1Q2_tmp"),true)
+    }
 
     val topicsSet = Set("cleaned_data")
     val kafkaParams = Map[String, String]("metadata.broker.list" -> "172.31.5.186:9092")
@@ -21,18 +32,24 @@ object G1Q2 {
       ssc, kafkaParams, topicsSet)
 
     initTable(sparkConf)
-    val mappingFunc = (airline: String, delay: Option[Double], state: State[OntimeSummary]) => {
-      val old_state = state.getOption.getOrElse(OntimeSummary(0, 0.0))
-      val new_state = OntimeSummary(old_state.count + 1, old_state.total_delay + delay.getOrElse(0.0))
-      state.update(new_state)
-      (airline, new_state.total_delay / new_state.count)
-    }
+    val updateFunction = 
+      (delays: Seq[Double], summary:Option[OntimeSummary]) => {
+        val state = summary.getOrElse(OntimeSummary(0, 0.0))
+        var count = state.count;
+        var delay = state.total_delay;
+        for (d <- delays) {
+          delay += d;
+          count += 1;
+        }
+        Some(OntimeSummary(count, delay));
+      }
 
     val query = messages
       .map(m => m._2.split('|'))
       .filter(fields => fields(8).length > 0 && fields(9).toDouble == 0.0 && fields(10).toDouble == 0.0)
-      .map(fields => (fields(1),fields(8).toDouble))
-      .mapWithState(StateSpec.function(mappingFunc))
+      .map(fields => (fields(1), fields(8).toDouble))
+      .updateStateByKey[OntimeSummary](updateFunction)
+      .map(s => (s._1, s._2.total_delay / s._2.count))
       .saveToCassandra("capstone", "airline_ontime_arrival")
 
     ssc.start()
@@ -49,7 +66,7 @@ object G1Q2 {
         DROP TABLE IF EXISTS capstone.airline_ontime_arrival
       """)
       session.execute(s"""
-        CREATE TABLE capstone.airline_ontime_arrival (airline TEXT PRIMARY KEY, arrival_delay DOUBLE)
+        CREATE TABLE capstone.airline_ontime_arrival (airline TEXT, arrival_delay DOUBLE, PRIMARY KEY (airline))
       """)
     }
   }
